430690d25a11d2bea5e1e96395499655f60cd07f
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-11-30 19:02:12 -0300

    feat: requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+SELECT
+    COUNT(s.song_id) cancoes,
+    COUNT(DISTINCT a.artist_id) artistas,
+    COUNT(DISTINCT al.album_id) albuns,
+FROM song AS s
+INNER JOIN album al ON s.album_id = al.album_id
+INNER JOIN artist a ON al.artist_id = a.artist_id;
