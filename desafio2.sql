0ff40e39de132cbc4a120154f184c8beeb85c2d9
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-11-30 19:56:42 -0300

    fix: Requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -2,6 +2,6 @@ SELECT
     COUNT(s.song_id) cancoes,
     COUNT(DISTINCT a.artist_id) artistas,
     COUNT(DISTINCT al.album_id) albuns,
-FROM song AS s
+FROM song s
 INNER JOIN album al ON s.album_id = al.album_id
 INNER JOIN artist a ON al.artist_id = a.artist_id;
