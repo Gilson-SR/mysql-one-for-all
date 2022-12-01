432334ccc7eca9977fa6db6126f7c9db10a0e5fe
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-11-30 22:35:39 -0300

    fix: Requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,9 @@
 SELECT
     a.artist_name artista,
     al.album_name album,
-    COUNT(f.artist_id) seguidores
+    COUNT(af.artist_id) seguidores
 FROM album AS al
 INNER JOIN artist AS a ON a.artist_id = al.artist_id
-INNER JOIN artist_follower AS af ON f.artist_id = a.artist_id
+INNER JOIN artist_follower AS af ON af.artist_id = a.artist_id
 GROUP BY al.album_id
 ORDER BY seguidores DESC,a.artist_name,al.album_name;
