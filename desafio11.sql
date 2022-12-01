8002717ac33a295828bb7cc15b3ac87a26a4311e
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-12-01 00:20:45 -0300

    feat: Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,16 @@
+SELECT 
+    song_name nome_musica,
+    CASE
+      WHEN song_name LIKE '%Bard%' THEN REPLACE(song_name,'Bard','QA')
+      WHEN song_name LIKE '%Amar%' THEN REPLACE(song_name,'Amar','Code Review')
+      WHEN song_name LIKE '%Pais%' THEN REPLACE(song_name,'Pais','Pull Requests')
+      WHEN song_name LIKE '%SOUL%' THEN REPLACE(song_name,'SOUL','CODE')
+      WHEN song_name LIKE '%SUPERSTAR%' THEN REPLACE(song_name,'SUPERSTAR','SUPERDEV')
+    END AS novo_nome
+FROM song
+WHERE song_name LIKE '%Bard%'
+OR song_name LIKE '%Amar%'
+OR song_name LIKE '%Pais%'
+OR song_name LIKE '%SOUL%'
+OR song_name LIKE '%SUPERSTAR%'
+ORDER BY song_name DESC;
